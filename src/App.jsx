d8b057e375d3e52e4f67aa144791401d80a55e23
shok1234 <shokhanyousif@gmail.com>
2026-04-02 00:18:53 +0300

    Portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        {/*hero*/}
+        <section>
+        <h1>Shokhan</h1>
+        <p>Frontend Developer | React</p>
+        </section>
+
+ {/*about*/}
+        <section>
+          <h2>About Me</h2>
+          <p>I am a frontend developer building web apps using React and JavaScript.</p>
+
+        </section>
+        {/*projects*/}
+        <section>
+  <h2>Projects</h2>
+
+  <div className="project-card">
+    <h3>React Weather App</h3>
+    <p>Weather app using React and Open-Meteo API</p>
+    <a href="https://github.com/shok1234/react-weather-app" target="_blank">
+      View Code
+    </a>
+  </div>
+</section>
+        {/*contact*/}
+        <section>
+          <h2>Contact</h2>
+          <p>Email: shokhanyousif@gmail.com</p>
+
+    <p>
+  🔗 <a href="https://github.com/shok1234" target="_blank">
+    GitHub Profile
+  </a>
+</p>
+        </section>
+
+      </div>
+    </>
+  )
+}
+
+export default App
